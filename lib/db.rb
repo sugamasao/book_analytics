aa4fa980cacb5c4367c57ba3dc16c0eeac4ce655
sugamasao <sugamasao@gmail.com>
2013-07-14 16:14:12 +0900

    add pooling size

--- lib/db.rb
+++ lib/db.rb
@@ -7,7 +7,7 @@ module DB
   # @return [ActiveRecord::ConnectionAdapters::SQLite3Adapter] connection object
   def self.connect_database(path = nil)
     spec = if ENV['DATABASE_URL']
-      ENV['DATABASE_URL']
+      ENV['DATABASE_URL'] + '?pool=15'
     else
       FileUtils.mkdir_p(path.dirname)
       {adapter: 'sqlite3', database: path.to_s}
