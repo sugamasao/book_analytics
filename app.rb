6cb57c7b6fe8538cff4b7d3c2bb61d462750b57f
sugamasao <sugamasao@gmail.com>
2013-07-14 17:27:29 +0900

    debug

--- app.rb
+++ app.rb
@@ -18,9 +18,9 @@ class Application < Sinatra::Base
   end
 
   get '/' do
-    @rank = Rank.order('update_date DESC').load
+    @rank = []#Rank.order('update_date DESC').load
     @book = Book.last
-    redirect '/how_to_start_up' if @rank.empty?
+    redirect '/how_to_start_up' if @book.empty?
     slim :index
   end
 
