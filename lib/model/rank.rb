e48d756a1d50ca1e28d4485f9d69f6d093b15f9d
sugamasao <sugamasao@gmail.com>
2013-07-14 15:52:46 +0900

    20 min step.

--- lib/model/rank.rb
+++ lib/model/rank.rb
@@ -7,7 +7,10 @@ class Rank < ActiveRecord::Base
   end
 
   def self.date_format(date)
-    Time.parse(date.strftime('%Y-%m-%d %H:00:00'))
+    str = date.strftime('%Y-%m-%d %H:')
+    min = date.min / 20 * 20
+    # 2013-10-11 12:34:55 => 2013-10-11 12:20:00
+    Time.parse("#{str}#{min}:00")
   end
 
   def local_update_date
