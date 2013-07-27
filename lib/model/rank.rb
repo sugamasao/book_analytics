72db030527280086b880e112d7012c3625ac04e8
sugamasao <sugamasao@gmail.com>
2013-07-27 13:05:49 +0900

    更新期間を一時間に一回にした

--- lib/model/rank.rb
+++ lib/model/rank.rb
@@ -8,10 +8,9 @@ class Rank < ActiveRecord::Base
   end
 
   def self.date_format(date)
-    str = date.strftime('%Y-%m-%d %H:')
-    min = date.min / 20 * 20
-    # 2013-10-11 12:34:55 => 2013-10-11 12:20:00
-    Time.parse("#{str}#{min}:00")
+    # 2013-10-11 12:34:55 => 2013-10-11 12:00:00
+    str = date.strftime('%Y-%m-%d %H:00:00')
+    Time.parse(str)
   end
 
   def local_update_date
